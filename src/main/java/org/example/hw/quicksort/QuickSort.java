2c70bd1bd7411bb8bc0d4313605b45ca46fdac6f
Anvar <kingartaved@yandex.ru>
2023-12-28 19:25:41 +0500

    Работа над быстрой сортировкой, план.

--- src/main/java/org/example/hw/quicksort/QuickSort.java
+++ src/main/java/org/example/hw/quicksort/QuickSort.java
@@ -2,4 +2,20 @@ package org.example.hw.quicksort;
 
 public class QuickSort {
 
+    public static void main(String[] args) {
+
+    }
+
+    public Integer[] divisionArrays(Integer[] insertArray) {
+        //опорный элемент определим как: (сначала если длина меньше 2, то возвращаем массив, как в слиянием)
+        // определим середину: если insertArray.length%2 !=0 (нечетное кол-во) => insertArray.length%2/2+1, иначе
+        // ((insertArray.length%2/2+1)+(insertArray.length%2/2-1))/2 - но так мы получим среднее между
+        // соседними элементами значение, но этого значения же нет в массиве, это просто среднее, подумать.
+
+    }
+
+    public Integer[] sortArrays(Integer[] leftArray, Integer[] rightArray){
+
+    }
+
 }
