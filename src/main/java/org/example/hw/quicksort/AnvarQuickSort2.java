a96e175502160f49c65c5b7e27ce92902253cc5e
Anvar <kingartaved@yandex.ru>
2024-01-05 20:57:51 +0500

    Второй вариант быстрой сортировки добит

--- src/main/java/org/example/hw/quicksort/AnvarQuickSort2.java
+++ src/main/java/org/example/hw/quicksort/AnvarQuickSort2.java
@@ -14,6 +14,10 @@ public class AnvarQuickSort2 {
         System.out.println();
         AnvarQuickSort2 anvarQuickSort2 = new AnvarQuickSort2();
         anvarQuickSort2.quickSort(array, 0, array.length - 1);
+        System.out.println("отсортированный массив: ");
+        for (Integer intElem : array) {
+            System.out.print(" " + intElem);
+        }
     }
 
     public void quickSort(Integer[] a, int leftIndex, int rightIndex) { //принимаем на вход массив, точку начала и точку конца сортировки массива
@@ -26,38 +30,43 @@ public class AnvarQuickSort2 {
         int pivotIndex = random.nextInt(leftIndex, rightIndex + 1); //определяем опорный элемент массива как случайный элемент диапазона
         System.out.println("random = " + pivotIndex);
         int pivot = a[pivotIndex];
-
+        System.out.println("pivot = " + pivot);
+        System.out.println();
 
         int l = leftIndex; //определим указатели с начальными значениями для левого и правого подмассивов
         int r = rightIndex;
-        System.out.printf("l = %s, r = %s ", l, r);
+        System.out.printf("Начальные l = %s, r = %s ", l, r);
         System.out.println();
 
-        System.out.println("pivot = " + pivot);
         System.out.println("вход в цикл while l <= r");
 
         while (l <= r) {
             while (a[l] < pivot) l++;
             while (a[r] > pivot) r--;
             if (l <= r) {
-                System.out.println("сработал if l <= r");
+                System.out.printf("при входе в if: l = %s, r = %s \n", l, r);
                 swap(a, l, r);
                 l++;
                 r--;
             }
-
-            System.out.printf("Новые: l = %s, r = %s ", l, r);
+//            System.out.println("после if: ");
             System.out.print("измененный массив: ");
             for (Integer intElem : a) {
                 System.out.print(" " + intElem);
             }
             System.out.println();
+            System.out.printf("Новые: l = %s, r = %s \n", l, r);
+
         }
         System.out.println("конец цикла while l <= r");
 
-        quickSort(a, leftIndex, l);
-        quickSort(a, r, rightIndex);
+        System.out.println("\nначало левой рекурсии");
+        quickSort(a, leftIndex, r);
+        System.out.println("конец левой рекурсии\n");
 
+        System.out.println("\nначало правой рекурсии");
+        quickSort(a, l, rightIndex);
+        System.out.println("конец правой рекурсии\n");
 
     }
 
